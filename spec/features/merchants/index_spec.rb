96d79f3f67982f4b73f1d6f1513be47043f1491a
Oscar Bellechasse <obellech10@gmail.com>
2019-06-25 09:32:25 -0600

    Fix merchant 2 variable

--- spec/features/merchants/index_spec.rb
+++ spec/features/merchants/index_spec.rb
@@ -13,23 +13,3 @@ RSpec.describe "Merchants index" do
     end
   end
 end
-
-
-
-# RSpec.describe "Songs Index" do
-#   describe "As a visitor" do
-#     it "I see all songs and their title and playcount" do
-#       song_1 = Song.create(title: "Hit me baby one more time", length: 345, playcount: 1000)
-#       song_2 = Song.create(title: "What is love", length: 543, playcount: 2)
-#
-#       visit "/songs"
-#
-#
-#
-#       expect(page).to have_content(song_1.title)
-#       expect(page).to have_content("Playcount: #{song_1.playcount}")
-#       expect(page).to have_content(song_2.title)
-#       expect(page).to have_content("Playcount: #{song_2.playcount}")
-#     end
-#   end
-# end
