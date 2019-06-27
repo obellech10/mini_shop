8cefec8628a45d643eb9386bbac9c4ba484f74c2
Oscar Bellechasse <obellech10@gmail.com>
2019-06-27 13:37:56 -0600

    Add routes for merchants_create merchants_update merchants-destory

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   get '/merchants', to: 'merchants#index'
   get '/merchants/new', to: 'merchants#new'
   get '/merchants/:id', to: 'merchants#show'
+  get '/merchants/:id/edit', to: 'merchants#edit'
   post '/merchants', to: 'merchants#create'
+  patch '/merchants/:id', to: 'merchants#update'
+  delete '/merchants/:id', to: 'merchants#destroy'
 end
